7efc1cf25d457b277a1a6ae9f200e02ba0260f4e
Asare Kwabena Ernest <asaern9@gmail.com>
2020-04-22 20:53:48 -0400

    ed2

--- 10687636_lab3.cpp
+++ 10687636_lab3.cpp
@@ -11,7 +11,7 @@ int main(int argc, char **argv){
 	int message = 0;
 	if(process_id != 0){
 		MPI_Recv(&message,1,MPI_INT,process_id-1,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-		cout<<"Process "<<process_id<<" received $ "<< message<<" from process "<<process_id-1<<endl;
+		cout<<"Process "<<process_id<<" received £ "<< message<<" from process "<<process_id-1<<endl;
 	}
 	else{
 		message = 666;
@@ -19,7 +19,7 @@ int main(int argc, char **argv){
 	MPI_Send(&message,1, MPI_INT,(process_id+1)%process_size,0,MPI_COMM_WORLD);
 	if(process_id == 0){
 		MPI_Recv(&message,1,MPI_INT,process_size-1,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-		cout<<"Process "<<process_id<<" received $ "<< message<<" from process "<<process_size-1<<endl;
+		cout<<"Process "<<process_id<<" received £ "<< message<<" from process "<<process_size-1<<endl;
 	}
 	MPI_Finalize();
 	return 0;
